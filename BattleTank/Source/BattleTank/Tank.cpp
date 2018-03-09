63d835836a638394b199d1efc367ef5af0637630
Trevor Orgill <trevororgill@weber.edu>
2018-03-09 15:19:33 -0700

    Init Episode 112 done

---
+++ BattleTank/Source/BattleTank/Tank.cpp
@@ -0,0 +1,85 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Tank.h"
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+
+
+// Sets default values
+ATank::ATank()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ATank::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATank::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+    PlayerInputComponent->BindAxis("Rotate_Turret", this, &ATank::RotateTurret);
+    PlayerInputComponent->BindAxis("Elevate_Barral", this, &ATank::ElevateBarral);
+    PlayerInputComponent->BindAxis("Rotate_Tank", this, &ATank::RotateTank);
+    PlayerInputComponent->BindAxis("Move_Tank", this, &ATank::MoveTank);
+}
+
+void ATank::ElevateBarral(float Speed) {
+    if (!Barral) { return; }
+    float NewSpeed = GetWorld()->DeltaTimeSeconds * Speed * RotationSpeed;
+    Barral->AddRelativeRotation(FRotator(NewSpeed, 0, 0));
+}
+
+void ATank::RotateTurret(float Speed)
+{
+    if (!Turret) { return; }
+    float NewSpeed = GetWorld()->DeltaTimeSeconds * Speed * RotationSpeed;
+    Turret->AddRelativeRotation(FRotator(0, NewSpeed, 0));
+}
+
+void ATank::MoveTank(float Speed)
+{
+    if (!Tank) { return; }
+    float NewSpeed = GetWorld()->DeltaTimeSeconds * Speed * MovementSpeed;
+//    Tank->AddRelativeRotation(FRotator(0, Speed, 0));
+    Tank->AddRelativeLocation(Tank->GetForwardVector() * NewSpeed);
+}
+
+void ATank::RotateTank(float Speed)
+{
+    if (!Tank) { return; }
+    float NewSpeed = GetWorld()->DeltaTimeSeconds * Speed * RotationSpeed;
+    Tank->AddRelativeRotation(FRotator(0, NewSpeed, 0));
+}
+
+void ATank::SetTurretChildActor(UChildActorComponent* TurretFromBP)
+{
+    if (!TurretFromBP) { return; }
+    Turret = TurretFromBP;
+}
+
+void ATank::SetBarralChildActor(UChildActorComponent* BarralFromBP)
+{
+    if (!BarralFromBP) { return; }
+    Barral = BarralFromBP;
+}
+
+void ATank::SetTankChildkActor(UStaticMeshComponent* TankFromBP)
+{
+    if (!TankFromBP) { return; }
+    Tank = TankFromBP;
+}
+
---
+++ BattleTank/Source/BattleTank/Tank.h
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Tank.generated.h"
+
+UCLASS()
+class BATTLETANK_API ATank : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ATank();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+    
+    UFUNCTION(BlueprintCallable, Category=Tank)
+    void SetTurretChildActor(UChildActorComponent* TurretFromBP);
+    
+    UFUNCTION(BlueprintCallable, Category=Tank)
+    void SetBarralChildActor(UChildActorComponent* BarralFromBP);
+    
+    UFUNCTION(BlueprintCallable, Category=Tank)
+    void SetTankChildkActor(UStaticMeshComponent* TankFromBP);
+    
+private:
+    void MoveTank(float Speed);
+    void RotateTank(float Speed);
+    void RotateTurret(float Speed);
+    void ElevateBarral(float Speed);
+    UStaticMeshComponent* Tank = nullptr;
+    UChildActorComponent* Turret = nullptr;
+    UChildActorComponent* Barral = nullptr;
+    
+    UPROPERTY(EditAnywhere)
+    float MovementSpeed = 120;
+    
+    UPROPERTY(EditAnywhere)
+    float RotationSpeed = 120;
+	
+};
