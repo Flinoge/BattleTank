64409d01a059cbfdf3f21fc7ef188c38d6aec24b
Trevor Orgill <trevororgill@weber.edu>
2018-03-19 08:01:39 -0600

    Episode 119 Done

---
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+
+void ATankPlayerController::BeginPlay() {
+    Super::BeginPlay();
+    
+    auto ControlledTank = GetControlledTank();
+    if (!ControlledTank) {
+        UE_LOG(LogTemp, Warning, TEXT("PlayerController not possesing a tank"));
+    } else {
+        UE_LOG(LogTemp, Warning, TEXT("PlayerController possessing: %s"), *(ControlledTank->GetName()));
+    }
+}
+
+void ATankPlayerController::Tick(float DeltaTime) {
+    Super::Tick(DeltaTime);
+    AimTowardsCrosshair();
+}
+
+ATank* ATankPlayerController::GetControlledTank() const {
+    return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::AimTowardsCrosshair() {
+    if (!GetControlledTank()) { return; }
+    
+    FVector HitLocation;
+    if (GetSightRayHitLocation(HitLocation)) {
+        
+    }
+}
+
+bool ATankPlayerController::GetSightRayHitLocation(FVector& HitLocation) const {
+    HitLocation = FVector(1.0);
+    return false;
+}
